305f7b049481df04597c6966cb971c7472f0e5e6
Ferdy Jonathan Angwijaya <ferdyjonathan12@gmail.com>
2024-09-22 02:11:43 +0700

    vercel

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
